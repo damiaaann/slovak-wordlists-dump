8bb422f0f8abc98d78d39d8fee8771e0464e8b40
damiaaann <>
2020-05-07 17:37:57 +0200

    added wiki dump wordlist in json

--- dicToJson.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs')
-var toJSON = require('plain-text-data-to-json')
- 
-var doc = fs.readFileSync('/home/wezeo/strankaExtension/skratky.txt', 'utf8')
- 
-var data = toJSON(doc)
- 
-fs.writeFileSync('output.json', JSON.stringify(data, null, 2) + '\n')
